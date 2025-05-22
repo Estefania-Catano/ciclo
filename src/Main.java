8f5f690b928d2a6edc183cd3e5954165c07287d0
Estefania-Catano <ecatano1@cesde.net>
2025-05-21 20:53:47 -0500

    cierre ciclo

--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,45 @@ public class Main {
         System.out.println("Ingrese la cantidad de notas a promediar: ");
         cantidadNotas = sc.nextDouble();
         System.out.println("Promedio de notas lógica programación");
-        for(int i=1; i<=cantidadNotas;i++){
-            System.out.print("Ingrese la nota: " + i + " ");
-            nota=sc.nextDouble();
-            acumulado+=nota;
+        /*Ciclo for*/
+//        for(int i=1; i<=cantidadNotas;i++){
+//            System.out.print("Ingrese la nota: " + i + " ");
+//            nota=sc.nextDouble();
+//            acumulado+=nota;
+//        }
+        /*Cilco while por defecto cuando se sabe el numero de repeticiones*/
+        int i=1;
+        while (i<=cantidadNotas){
+            System.out.print("Ingrese la nota "+ i +": ");
+            nota = sc.nextDouble();
+            acumulado+= nota;
+            i++;
         }
+        /*Ciclo controlado por un centinela, bandera o interruptor cuando no se sabe la cantidad de veces a repetir*/
+
+//        int centinela=1;
+//        int opcion=0;
+//        int i=1;
+//        while (centinela==1){
+//            System.out.print("Ingrese la nota "+i+": ");
+//            nota = sc.nextDouble();
+//            acumulado = nota;
+//            i++;
+//            System.out.println("Desea registrar otra nota?\n1 - Si \n0 - No");
+//            opcion = sc.nextInt();
+//            if(opcion==0){
+//                centinela=0;
+//            }
+//        }
+//        System.out.println(i);
+
+//        int i =10;
+//      do{
+//          System.out.print("Ingrese la nota "+ i +": ");
+//            nota = sc.nextDouble();
+//            acumulado+= nota;
+//          i++;
+//      } while (i<=5);
         System.out.println("El promedio del estudiante es: " + acumulado/cantidadNotas);
     }
 }
